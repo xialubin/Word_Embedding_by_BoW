dd45787e9d83fd574fe531de6e7f1ad2f8b1cf6e
xialubin <18511438015@163.com>
2018-09-15 10:49:10 +0800

    the complete version

--- bow_embedding.py
+++ bow_embedding.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 CONTEXT_SIZE = 2  # 2 words to the left, 2 to the right
+EMBEDDING_DIM = 10
 raw_text = """We are about to study the idea of a computational process.
 Computational processes are abstract beings that inhabit computers.
 As they evolve, processes manipulate other abstract things called data.
@@ -22,7 +23,7 @@ for i in range(2, len(raw_text) - 2):
                raw_text[i + 1], raw_text[i + 2]]
     target = raw_text[i]
     data.append((context, target))
-print(data[:5])
+# print(data[:5])
 
 
 class CBOW(nn.Module):
@@ -30,7 +31,7 @@ class CBOW(nn.Module):
     def __init__(self, vocab_size, embedding_dim, context_size):
         super(CBOW, self).__init__()
         self.embeddings = nn.Embedding(vocab_size, embedding_dim)
-        self.linear1 = nn.Linear(context_size*embedding_dim, 128)
+        self.linear1 = nn.Linear(2*context_size*embedding_dim, 128)
         self.linear2 = nn.Linear(128, vocab_size)
 
     def forward(self, inputs):
@@ -49,4 +50,45 @@ def make_context_vector(context, word_to_ix):
     return torch.tensor(idxs, dtype=torch.long)
 
 
-make_context_vector(data[0][0], word_to_ix)  # example
+# make_context_vector(data[0][0], word_to_ix)  # example
+losses = []
+loss_function = nn.NLLLoss()
+model = CBOW(vocab_size, EMBEDDING_DIM, CONTEXT_SIZE)
+optimizer = optim.SGD(model.parameters(), lr=0.001)
+device = torch.device("cuda:0")
+model.to(device)
+
+for epoch in range(100):
+    total_loss = 0
+    for context, target in data:
+
+        # Step 1. Prepare the inputs to be passed to the model (i.e, turn the words
+        # into integer indices and wrap them in tensors)
+        context_ids = make_context_vector(context, word_to_ix)
+        context_ids = context_ids.to(device)
+
+        # Step 2. Recall that torch *accumulates* gradients. Before passing in a
+        # new instance, you need to zero out the gradients from the old
+        # instance
+        model.zero_grad()
+
+        # Step 3. Run the forward pass, getting log probabilities over next
+        # words
+        log_probs = model.forward(context_ids)
+
+        # Step 4. Compute your loss function. (Again, Torch wants the target
+        # word wrapped in a tensor)
+        ground_true = make_context_vector([target], word_to_ix).to(device)
+        loss = loss_function(log_probs, ground_true)
+
+        # Step 5. Do the backward pass and update the gradient
+        loss.backward()
+        optimizer.step()
+
+        # Get the Python number from a 1-element Tensor by calling tensor.item()
+        total_loss += loss.item()
+    losses.append(total_loss)
+    print(total_loss)
+print(losses)
+
+
